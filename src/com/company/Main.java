d44a541b47010592484245dcc75a84f46df8356b
Popescu Emanuel <emyl_ppsc@yahoo.com>
2020-09-23 12:40:09 +0300

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main extends OptionMenu{
+
+    public static void main(String[] args) {
+        OptionMenu optionMenu = new OptionMenu();
+        optionMenu.getLogin();
+    }
+}
